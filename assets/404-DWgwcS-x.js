e5b87fdcd770a2fa73f59cf8caf2bc27a2e0af7b
letheky <thekyle997@gmail.com>
2024-07-03 09:29:19 +0700

    Updates

--- assets/404-DWgwcS-x.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-JQteVaZ1.js";const o={};function t(r,_){return c(),n("div",null," Page not found ")}const s=e(o,[["render",t]]);export{s as default};
---
+++ assets/Auth-Cli_sGs-.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-CxV1hezB.js";const n={};function o(r,a){return c(),t("div",null," Auth page ")}const _=e(n,[["render",o]]);export{_ as default};
