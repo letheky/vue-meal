be9a96e13cd26ba79ac5cb32a37ec8dbe2b4776d
letheky <thekyle997@gmail.com>
2024-07-03 09:59:28 +0700

    Updates

--- assets/SearchByIngredient-JitYwG_n.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as n,c}from"./index-zTDdU6Ir.js";const r={};function t(o,a){return n(),c("div",null," Meal By Ingredient ")}const _=e(r,[["render",t]]);export{_ as default};
---
+++ assets/SearchByLetter-CPFM3RkN.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-Cx8fH9Nv.js";const r={};function n(o,a){return t(),c("div",null," Meal By Letter ")}const _=e(r,[["render",n]]);export{_ as default};
